1bfae47c6ec0e1a2189214ae03025e5f26029bfa
driwell <driwell@tuta.io>
2025-01-07 21:36:50 +0000

    enable collision between the ball, paddle and walls

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    math::bounding::{Aabb2d, BoundingCircle, BoundingVolume, IntersectsVolume},
+    prelude::*,
+};
 
 const PADDLE_SIZE: Vec2 = Vec2::new(20., 120.);
 const PADDLE_COLOR: Color = Color::srgb(0.898, 0.784, 0.565);
@@ -23,7 +26,10 @@ const BALL_COLOR: Color = Color::srgb(1.0, 0.5, 0.5);
 pub struct Paddle;
 
 #[derive(Component)]
-struct Collider;
+pub struct Collider;
+
+#[derive(Event, Default)]
+pub struct CollisionEvent;
 
 #[derive(Bundle)]
 struct WallBundle {
@@ -79,7 +85,7 @@ impl WallBundle {
 }
 
 #[derive(Component)]
-struct Ball;
+pub struct Ball;
 
 #[derive(Component, Deref, DerefMut)]
 pub struct Velocity(Vec2);
@@ -147,4 +153,74 @@ pub fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time: Res<T
         transform.translation.x += velocity.x * time.delta_secs();
         transform.translation.y += velocity.y * time.delta_secs();
     }
+}
+
+pub fn check_for_collisions(
+    ball_query: Single<(&mut Velocity, &Transform), With<Ball>>,
+    collider_query: Query<(Entity, &Transform), With<Collider>>,
+    mut collision_events: EventWriter<CollisionEvent>,
+) {
+    let (mut ball_velocity, ball_transform) = ball_query.into_inner();
+
+    for collider_transform in &collider_query {
+        let collision = ball_collision(
+            BoundingCircle::new(ball_transform.translation.truncate(), BALL_DIAMETER / 2.),
+            Aabb2d::new(
+                collider_transform.1.translation.truncate(),
+                collider_transform.1.scale.truncate() / 2.,
+            ),
+        );
+
+        if let Some(collision) = collision {
+            collision_events.send_default();
+
+            let mut reflect_x = false;
+            let mut reflect_y = false;
+
+            match collision {
+                Collision::Left => reflect_x = ball_velocity.x > 0.0,
+                Collision::Right => reflect_x = ball_velocity.x < 0.0,
+                Collision::Top => reflect_y = ball_velocity.y < 0.0,
+                Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
+            }
+
+            if reflect_x {
+                ball_velocity.x = -ball_velocity.x;
+            }
+
+            if reflect_y {
+                ball_velocity.y = -ball_velocity.y;
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum Collision {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+fn ball_collision(ball: BoundingCircle, bounding_box: Aabb2d) -> Option<Collision> {
+    if !ball.intersects(&bounding_box) {
+        return None;
+    }
+
+    let closest = bounding_box.closest_point(ball.center());
+    let offset = ball.center() - closest;
+    let side = if offset.x.abs() > offset.y.abs() {
+        if offset.x < 0. {
+            Collision::Left
+        } else {
+            Collision::Right
+        }
+    } else if offset.y > 0. {
+        Collision::Top
+    } else {
+        Collision::Bottom
+    };
+
+    Some(side)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
-use padler::{apply_velocity, move_paddle, setup};
+use padler::{apply_velocity, check_for_collisions, move_paddle, setup, CollisionEvent};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
+        .add_event::<CollisionEvent>()
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (apply_velocity, move_paddle))
+        .add_systems(
+            FixedUpdate,
+            (apply_velocity, move_paddle, check_for_collisions),
+        )
         .run();
 }
