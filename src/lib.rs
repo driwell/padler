0bea8098a718138d179f8f9fb015eed9a6f0cfa5
driwell <driwell@tuta.io>
2025-01-07 00:54:12 +0000

    move the paddle with input

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,16 @@ use bevy::prelude::*;
 
 const PADDLE_SIZE: Vec2 = Vec2::new(20., 120.);
 const PADDLE_COLOR: Color = Color::srgb(0.898, 0.784, 0.565);
-const GAP_BETWEEN_PADDLE_AND_WALL: f32 = 10.;
+const PADDLE_SPEED: f32 = 500.;
+const PADDLE_X_MARGIN: f32 = 10.;
+const PADDLE_Y_MARGIN: f32 = 10.;
 
 const WALL_X: f32 = 350.;
+const WALL_Y: f32 = 450.;
+const WALL_THICKNESS: f32 = 10.;
 
 #[derive(Component)]
-struct Paddle;
+pub struct Paddle;
 
 #[derive(Component)]
 struct Collider;
@@ -15,7 +19,7 @@ struct Collider;
 pub fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 
-    let paddle_x = (WALL_X * -1.) + GAP_BETWEEN_PADDLE_AND_WALL;
+    let paddle_x = (WALL_X * -1.) + PADDLE_X_MARGIN;
 
     commands.spawn((
         Sprite::from_color(PADDLE_COLOR, Vec2::ONE),
@@ -27,4 +31,30 @@ pub fn setup(mut commands: Commands) {
         Paddle,
         Collider,
     ));
+}
+
+pub fn move_paddle(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut paddle_transform: Single<&mut Transform, With<Paddle>>,
+    time: Res<Time>,
+) {
+    let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::ArrowDown) {
+        direction -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        direction += 1.0;
+    }
+
+    // Calculate the new horizontal paddle position based on player input
+    let new_paddle_position =
+        paddle_transform.translation.y + direction * PADDLE_SPEED * time.delta_secs();
+
+    // Update the paddle position,
+    // making sure it doesn't cause the paddle to leave the arena
+    let left_bound = (WALL_Y * -1.) + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_Y_MARGIN;
+    let right_bound = WALL_Y - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_Y_MARGIN;
+    paddle_transform.translation.y = new_paddle_position.clamp(left_bound, right_bound);
 }
