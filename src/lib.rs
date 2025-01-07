ae05a016c3e14236b1c13a551afb767941effc1c
driwell <driwell@tuta.io>
2025-01-07 00:18:58 +0000

    normalize floats

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 
-const PADDLE_SIZE: Vec2 = Vec2::new(20.0, 120.0);
+const PADDLE_SIZE: Vec2 = Vec2::new(20., 120.);
 const PADDLE_COLOR: Color = Color::srgb(0.898, 0.784, 0.565);
-const GAP_BETWEEN_PADDLE_AND_WALL: f32 = 10.0;
+const GAP_BETWEEN_PADDLE_AND_WALL: f32 = 10.;
 
 const WALL_X: f32 = 350.;
 
@@ -20,8 +20,8 @@ pub fn setup(mut commands: Commands) {
     commands.spawn((
         Sprite::from_color(PADDLE_COLOR, Vec2::ONE),
         Transform {
-            translation: Vec3::new(paddle_x, 0.0, 0.0),
-            scale: PADDLE_SIZE.extend(1.0),
+            translation: Vec3::new(paddle_x, 0., 0.),
+            scale: PADDLE_SIZE.extend(1.),
             ..default()
         },
         Paddle,
