8ed92f33a58a622f6e5f64cbc4635033526bf8cd
driwell <driwell@tuta.io>
2025-01-07 00:16:52 +0000

    draw the player paddle on the left side

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+const PADDLE_SIZE: Vec2 = Vec2::new(20.0, 120.0);
+const PADDLE_COLOR: Color = Color::srgb(0.898, 0.784, 0.565);
+const GAP_BETWEEN_PADDLE_AND_WALL: f32 = 10.0;
+
+const WALL_X: f32 = 350.;
+
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Component)]
+struct Collider;
+
+pub fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    let paddle_x = (WALL_X * -1.) + GAP_BETWEEN_PADDLE_AND_WALL;
+
+    commands.spawn((
+        Sprite::from_color(PADDLE_COLOR, Vec2::ONE),
+        Transform {
+            translation: Vec3::new(paddle_x, 0.0, 0.0),
+            scale: PADDLE_SIZE.extend(1.0),
+            ..default()
+        },
+        Paddle,
+        Collider,
+    ));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
+use padler::setup;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
+        .add_systems(Startup, setup)
+        .run();
 }
